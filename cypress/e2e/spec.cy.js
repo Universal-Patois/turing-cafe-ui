7bdf13bf2b39656a38f85755e9dcfb3314214395
Andrew Knapick <ajknapick@yahoo.com>
2022-10-31 11:54:36 -0600

    Wrote test for making a reservation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -21,11 +21,16 @@ describe('cafe test', () => {
     cy.get('h3').contains("12/29")
   })
 
-  it('should')
+  it('should display a new reservation after it is made', () => {
+   cy.get('[placeholder="Name"]').type('Andrew').should('have.value', 'Andrew')
+   cy.get('[placeholder="Date(mm/dd)"]').type('10/16').should('have.value', '10/16')
+   cy.get('[placeholder="Time"]').type(730).should('have.value', '730')
+   cy.get('[type="number"]').type(2).should('have.value', 2)
+   cy.get('.form-button').click()
+   cy.get('h2').contains('Andrew')
+   cy.get('h3').contains('10/16')
+    
+  })
 
 })
 //* Write a test that checks that when data is put into the form, the value is reflected in that form input.
-
-
-
-//* Write a test to check the user flow of adding a new reservation to the page.
