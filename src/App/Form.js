3c9af2a849286ad4715d33fdac62f47be5f3a26a
Andrew Knapick <ajknapick@yahoo.com>
2022-10-31 10:24:44 -0600

    Create Form component with inputs

---
+++ src/App/Form.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import './Form.css'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      guests: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
+
+  render() {
+    return (
+      <form>
+        <input 
+          type='text'
+          placeholder="Name"
+          name='name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input 
+          type='text'
+          placeholder='Date(mm/dd)'
+          name='date'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input 
+          type='text'
+          placeholder="Time"
+          name='time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input 
+          type='text'
+          placeholder="Number of Guests"
+          name='guests'
+          value={this.state.guests}
+          onChange={event => this.handleChange(event)}
+        />
+
+        
+
+      </form>
+    )
+  }
+}
